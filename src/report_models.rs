9c8a5adfcc6a727381942747466b178cfc84dd06
PsychedelicShayna <34726837+PsychedelicShayna@users.noreply.github.com>
2023-10-21 01:12:03 -0300

    Refactored report models.
    Should have been part of the last commit ngl.

--- src/report_models.rs
+++ src/report_models.rs
@@ -5,33 +5,33 @@ use serde::{Deserialize, Serialize};
 pub type TimestampUnix = u64;
 pub type TimestampUTC = String;
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct TimelineEntry {
-    pub time_now: TimestampUnix,
-    pub time_old: TimestampUnix,
-    pub time_delta: TimestampUnix,
+    pub timestamp: TimestampUnix,
+    pub paststamp: TimestampUnix,
+    pub pastdelta: TimestampUnix,
 
-    pub new_unique: u64,
-    pub new: u64,
+    pub views_u: u64,
+    pub views_a: u64,
 
-    pub delta: u64,
-    pub delta_unique: u64,
+    pub change_a: i64,
+    pub change_u: i64,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct Timeline {
     pub metrics_total: Metric,
-    pub newest_metric: TimestampUnix,
+    pub latest_entry_timestamp: TimestampUnix,
     pub metric_timeline: HashMap<TimestampUnix, TimelineEntry>,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct Metric {
     pub all: u64,
     pub unique: u64,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct ViewCloneMetric {
     pub views: Metric,
     pub clones: Metric,
@@ -41,17 +41,17 @@ pub type ReferrerDomain = String;
 pub type ContentPath = String;
 pub type Map<K, V> = HashMap<K, V>;
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct Report {
     pub total: ViewCloneMetric,
     pub referrals: Metric,
     pub content: Metric,
     pub referrers: u64,
-    pub paths: u64,
+    pub traffic_paths: u64,
 
     pub weekly: HashMap<TimestampUTC, ViewCloneMetric>,
     pub daily: HashMap<TimestampUTC, ViewCloneMetric>,
 
-    pub referrer_timelines: Map<ReferrerDomain, Timeline>,
-    pub content_timelines: Map<ContentPath, Timeline>,
+    pub timelines_referrals: Map<ReferrerDomain, Timeline>,
+    pub timelines_content_traffic: Map<ContentPath, Timeline>,
 }
