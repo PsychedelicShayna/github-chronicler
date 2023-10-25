e6f186c4894b5432373ff5c2ac0740d93a27eb93
PsychedelicShayna <34726837+PsychedelicShayna@users.noreply.github.com>
2023-10-25 08:20:12 -0300

    Some more time functions, needed later.
    These are gonna be necessary later on.

--- src/timecalc.rs
+++ src/timecalc.rs
@@ -1,10 +1,8 @@
 use anyhow as ah;
-use chrono::{
-    format::{parse, Parsed},
-    prelude::*,
-};
+use chrono::{prelude::*, Duration};
+use std::time::{SystemTime, UNIX_EPOCH};
 
-pub fn get_day_utc() -> ah::Result<String> {
+pub fn get_utc_datestamp() -> ah::Result<String> {
     let utc_now = Utc::now();
 
     let midnight_aligned = Utc
@@ -19,6 +17,25 @@ pub fn get_day_utc() -> ah::Result<String> {
             ))
         })
         .map(|time| time.format("%Y-%m-%dT%H:%M:%SZ").to_string())
+}
+
+pub fn next_utc_day() -> ah::Result<Duration> {
+    let now = Utc::now();
+
+    match Utc.with_ymd_and_hms(now.year(), now.month(), now.day() + 1, 0, 0, 0) {
+        chrono::LocalResult::None => ah::bail!(""),
+        chrono::LocalResult::Single(r) => Ok(r),
+        chrono::LocalResult::Ambiguous(r1, _) => Ok(r1),
+    }
+    .map(|t| t.signed_duration_since(now))
+}
+
+pub fn get_unix_timestamp() -> u64 {
+    let duration_since_epoch = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("SystemTime before Unix epoch!");
+
+    duration_since_epoch.as_secs()
 }
 
 pub fn subtract_two_weeks(timestamp: &String) -> ah::Result<String> {
